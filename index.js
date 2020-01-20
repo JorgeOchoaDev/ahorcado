a298070fe5aebc35d3a99dea0e8084d85a73d9bf
JorgeOchoaDev <jl91ob@protonmail.com>
2020-01-20 17:06:20 -0600

    ver. 1.00

---
+++ index.js
@@ -0,0 +1,13 @@
+/*
+* Quiero programar un juego de ahorcado.
+* Mi palabra a adivinar será una variable que llamaré "palabra"
+* Necesito una función adivinar() la cual reciba una letra y me responda si mi palabra contiene esa letra
+* Ej: si palabra = "ejemplo" y yo pido adivinar("o") me debe responder "La letra o se encuentra en la palabra"
+* 
+*/
+
+var palabra
+
+function adivinar () {
+
+}
