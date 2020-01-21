1f7cd0499dc5837f94144003af755f92c0b2f493
JorgeOchoaDev <jl91ob@protonmail.com>
2020-01-20 18:57:35 -0600

    added instructions

--- index.js
+++ index.js
@@ -2,8 +2,11 @@
 * Quiero programar un juego de ahorcado.
 * Mi palabra a adivinar será una variable que llamaré "palabra"
 * Necesito una función adivinar() la cual reciba una letra y me responda si mi palabra contiene esa letra
-* Ej: si palabra = "ejemplo" y yo pido adivinar("o") me debe responder "La letra o se encuentra en la palabra"
-* 
+* Ej: si palabra = "ejemplo" y yo pido adivinar("o"), me debe responder "La letra o se encuentra en la palabra!"
+* Complicación 1: Necesito que me diga el lugar donde coincide mi letra con la palabra.
+* Ej: si palabra = "ejemplo" y yo pido adivinar("o"), me debe responder "La letra o se encuentra en el lugar 6 de la palabra"
+* Complicación 2 : Necesito que me diga todas las coincidencias y sus lugares si mi letra coincide más de una vez
+* Ej: si palabra = "control" y yo pido adivinar("o"), me debe responder "La letra o se encuentra en los lugares 1 y 5 de la palabra" 
 */
 
 var palabra
